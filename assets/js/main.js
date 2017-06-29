2a52c3c89635365b7d5f4223b18698d89a1135f2
KarinaLaroze <karinalaroze@gmail.com>
2017-06-29 10:49:08 -0400

    agregando tareas con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	
+ $("button").click(function(e){
+		e.preventDefault();
+		var name = $("#tareas").val();
+		var message = $("#agregar").val();
+		if(name == "" && message == ""){
+			alert("Debes añadir tareas");
+		}else{
+			$('#lista').append('<div><span class="center-align">'+ name + '</span><p class="center-align">' + message + '</p></div>');
+		}
+	});
+
 })
